1b0cefa2930d69d24cf01e4ed11d3146eb4e014c
lcq <lee-cq@qq.com>
2025-11-05 17:30:34 +0800

    3. 更新版本号生成机制

--- translate/__init__.py
+++ translate/__init__.py
@@ -6,12 +6,23 @@
 @Date-Time  : 2025/10/14 22:23
 """
 
-VERSION = "1.0.0"
+
+def get_version():
+    _main_split = UPDATE_LOG.strip().split('\n\n')[0].split('\n')
+    _m, _fix_log = _main_split[0], _main_split[-1]
+    return f"{_m}.{_fix_log.split('.')[0]}"
+
 
 UPDATE_LOG = """
-1.1.0
+1.1
 1. 添加ROW REQUEST日志方便分析Translate API的原始记录
 2. 防止调整translate窗口时的无限递归
+3. 更新版本号生成机制
 
 1.0.0
 """
+
+VERSION = get_version()
+
+if __name__ == '__main__':
+    print(get_version())
